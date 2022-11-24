5685cfc943ca1c0a18c5f498d7da609235ee7475
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-11-24 19:14:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-   return 0;
+int main()
+{
+    int n=0,a=1,b=1,c=0;
+    cin>>n;
+    if(n>=2) 
+    {
+     cout<<a<<endl;
+     cout<<b<<endl;
+     for (int i=2;i<n;i++)
+     {
+         c=a+b;
+          a=b;
+          b=c;
+          cout << c << endl;
+     }
+    }
+    else 
+    {
+        cout<<"errore"<< endl;
+    }
+    
+    return 0;
 }
