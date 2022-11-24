cfd56325355a020e0a20b2e7f736744e55c14638
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-11-24 19:18:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,20 @@
-int main()
-{
-    int n=0,a=1,b=1,c=0;
-    cin>>n;
-    if(n>=2) 
-    {
-     cout<<a<<endl;
-     cout<<b<<endl;
-     for (int i=2;i<n;i++)
-     {
-         c=a+b;
-          a=b;
-          b=c;
-          cout << c << endl;
-     }
-    }
-    else 
-    {
-        cout<<"errore"<< endl;
+#include <iostream>
+using namespace std;
+
+int main() {
+   int b = 0, a = 1, c = 0, n = 0;
+    cin >> n;
+    for (int i=1; i>0; i++) {
+        c = b + a;
+        if (c <= n) {
+            a = b;
+            b = c;
+            cout << c << endl;
+        } else {
+            break;
+        }
     }
     
-    return 0;
+return 0;
+   return 0;
 }
